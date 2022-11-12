565e9361106924d885f4962f1dfc82303434af50
mushoffa <ridwan.mushoffa@gmail.com>
2022-11-12 12:42:42 +0700

    Putting it all together

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	
+	"time"
+
+	"shorten-url-go/data/controller"
+	"shorten-url-go/data/repository"
+	"shorten-url-go/domain/usecase"
+	"shorten-url-go/infrastructure/memory"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func main() {
+	repository := repository.NewRepository(memory.NewURLMemory())
+	usecase := usecase.NewUsecase(repository)
+	controller := controller.NewURLController(usecase)
 
+	server := fiber.New()
+
+	server.Use(limiter.New(limiter.Config{
+		Expiration: 5 * time.Second,
+		Max: 5,
+	}))
+
+	controller.Router(server)
+
+	server.Listen(":9090")
 }
