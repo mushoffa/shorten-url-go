d9b8b8278fc36f2529b9d6a6eb8e8798189536cb
mushoffa <ridwan.mushoffa@gmail.com>
2022-11-12 12:42:02 +0700

    feat(Usecase): Add Business Logic Process

--- domain/usecase/generator.go
+++ domain/usecase/generator.go
@@ -5,12 +5,19 @@ import (
 	"time"
 )
 
+// Size of shorten URL
 const size = 6
 
+// Characters of shorten URL in alphanumeric
 var runes = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
+
+// Logic functiono to generate unique alias of shorten URL
 func GenerateShortURL() string {
+
+	// Seed for the randomizer to genereate random number everytime this function is called
 	rand.Seed(time.Now().UnixNano())
+
 	chars := make([]rune, size)
 
 	for i := range chars {
---
+++ domain/usecase/usecase.go
@@ -0,0 +1,93 @@
+package usecase
+
+import (
+	"fmt"
+	"errors"
+	"regexp"
+	"strings"
+	"time"
+
+	"shorten-url-go/domain/entity"
+	"shorten-url-go/domain/repository"
+)
+
+type Usecase interface {
+	EncodeURL(string) (string, error)
+	DecodeURL(string) (string, error)
+	GetURL(string) (entity.URL, error)
+	GetAllURL() ([]entity.URL, error)
+	ValidateURL(string) error
+}
+
+type usecase struct {
+	repository domain.Repository
+}
+
+func NewUsecase(repository domain.Repository) Usecase {
+	return &usecase{repository}
+}
+
+func (u *usecase) EncodeURL(originalURL string) (string, error) {
+	if err := u.ValidateURL(originalURL); err != nil {
+		return "", err
+	}
+
+	existingShortenURL, err := u.repository.FindByOriginalURL(originalURL)
+	if err == nil {
+		return existingShortenURL, nil
+	}
+
+	shortenURL := GenerateShortURL()
+
+	now := time.Now().Local()
+	timestamp := fmt.Sprintf("%d/%d/%d %02d:%02d:%02d", now.Day(), now.Month(), now.Year(), now.Hour(), now.Minute(), now.Second())
+
+	url := entity.URL {
+		Created: timestamp,
+		OriginalURL: originalURL,
+		ShortenURL: shortenURL,
+		RedirectCount: 0,
+	}
+
+	u.repository.AddURL(&url)
+
+	return shortenURL, nil
+}
+
+func (u *usecase) DecodeURL(url string) (string, error) {
+	data, err := u.repository.FindURL(url)
+	if err != nil {
+		return "", err
+	}
+
+	data.RedirectCount += 1
+
+	return data.OriginalURL, nil
+}
+
+func (u *usecase) GetURL(url string) (entity.URL, error) {
+	data, err := u.repository.FindURL(url)
+	if err != nil {
+		return entity.URL{}, err
+	}
+
+	return *data, nil
+}
+
+func (u *usecase) GetAllURL() ([]entity.URL, error) {
+	return u.repository.FindAllURL()
+}
+
+func (u *usecase) ValidateURL(url string) error {
+	regex, err := regexp.Compile("^(http|https)://")
+	if err != nil {
+		return err
+	}
+
+	url = strings.TrimSpace(url)
+	if regex.MatchString(url) {
+		return nil
+	}
+
+	return errors.New("Invalid format, URL must begin with http or https")
+}
